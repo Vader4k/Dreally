38a533effd5cfb6801fefa4bf5d2bce1619d072d
danie odukwe <kingdanny295@gmail.com>
2024-04-02 18:07:26 +0100

    @add/ import all images in an index file to help with clean imports in component

---
+++ src/assets/index.js
@@ -0,0 +1,44 @@
+// hero section images
+import first from './Ellipse 1.png'
+import second from './Ellipse 2.png'
+import third from './Ellipse 3.png'
+import fourth from './Ellipse 4.png'
+import fifth from './Ellipse 5.png'
+
+// why dreally icons
+import idea from './idea.png'
+import link from './link.png'
+import security from './security.png'
+
+// service icons
+import animations from './animations.png'
+import setting from './cutomize.png'
+import tag from './tag.png'
+import customize from './customize.png'
+import live from './live.png'
+
+//footer icons
+import insta from './insta.png'
+import twitter from './twitter.png'
+import tiktok from './tiktok.png'
+import youtube from './youtube.png'
+
+export {
+    first,
+    second,
+    third,
+    fourth,
+    fifth,
+    idea,
+    link,
+    security,
+    animations,
+    setting,
+    tag,
+    customize,
+    live,
+    insta,
+    twitter,
+    tiktok,
+    youtube,
+}
