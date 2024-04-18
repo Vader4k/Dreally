784ad8103256fc3c6da729cfbe0ea5ad546ed83f
danie odukwe <kingdanny295@gmail.com>
2024-04-18 22:13:48 +0100

    @add/ created a scroll to top function to always take me to the top of a page when i change routes

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -3,10 +3,12 @@ import Home from '../pages/Home'
 import Discover from '../pages/Discover'
 import Marketplace from '../pages/Marketplace'
 import About from "../pages/About"
+import ScrollToTop from '../components/ScrollToTop'
 
 const Routing = () => {
   return (
     <section>
+      <ScrollToTop />
       <Routes>
         <Route index element={<Home />} />
         <Route path="/discover" element={<Discover />} />
