8923fd9b74f04fede29a86c1b8bbe19488c7d5d9
danie odukwe <kingdanny295@gmail.com>
2024-04-19 18:43:20 +0100

    @updates/ made accordion responsive

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -5,9 +5,9 @@ import { Accordion } from '../components'
 const Features = () => {
   return (
     <section className="my-20 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-40">
-      <h1 className="font-bold text-4xl ">All Available Features</h1>
+      <h1 className="font-bold text-2xl lg:text-4xl ">All Available Features</h1>
 
-      <div className="my-10">
+      <div className="my-10 w-full">
         <Accordion items={features}/>
       </div>
     </section>
