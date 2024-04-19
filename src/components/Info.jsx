c0c079f6ea35830401f7bbf73b067470cf6e1e97
danie odukwe <kingdanny295@gmail.com>
2024-04-19 18:17:46 +0100

    @update/ made info section responsive

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,7 +7,7 @@ import 'swiper/css/bundle';
 
 const Info = () => {
   return (
-    <section className='w-full flex items-center justify-evenly gap-14 pt-28 py-10 px-20'>
+    <section className='w-full flex flex-col-reverse lg:flex-row items-center justify-evenly gap-6 lg:gap-14 pt-28 py-10 lg:px-20'>
         <div className='w-full p-5 flex items-center justify-center overflow-hidden'>
             <Swiper
                 className='w-full rounded-3xl overflow-hidden'
@@ -19,19 +19,19 @@ const Info = () => {
                 }}
                 >
                 <SwiperSlide>
-                    <img className='cursor-pointer w-[600px]' src={img1} alt="image 1" />
+                    <img className='cursor-pointer w-full max-w-[600px]' src={img1} alt="image 1" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='cursor-pointer w-[600px]' src={img2} alt="image 2" />
+                    <img className='cursor-pointer w-full max-w-[600px]' src={img2} alt="image 2" />
                 </SwiperSlide>
             </Swiper>
         </div>
 
-        <div className='w-full p-5'>
-            <h1 className='font-extrabold text-[3rem]'>
+        <div className='w-full p-5 '>
+            <h1 className='font-extrabold text-[2rem] lg:text-[3rem]'>
                 Craft and personalize your Dreally profile in minutes
             </h1>
-            <p className='text-neutral-300 leading-6 py-4'>
+            <p className='text-neutral-300 leading-6 py-6'>
                 Welcome to Dreally, your ultimate networking companion! We{"'"}re here to revolutionize the way you connect, showcase your talents, and elevate your professional presence online. With sleek digital business cards, customizable portfolios, and seamless networking tools, Dreally empowers you to make meaningful connections and stand out in a crowded digital landscape. Join us and unlock new opportunities for success. Together, let{"'"}s redefine networking
             </p>
             <button className=' px-16 py-4 bg-primary-500 border-none text-white rounded-[100px] cursor-pointer'>
