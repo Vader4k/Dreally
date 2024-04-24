5cc499931174ab16f66e27caa3dbbec3ed355dab
danie odukwe <kingdanny295@gmail.com>
2024-04-24 11:09:00 +0100

    @fix/ removed unused component

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from "react-router-dom"
 import Home from '../pages/Home'
-import Discover from '../pages/Discover'
-import Marketplace from '../pages/Marketplace'
 import About from "../pages/About"
 import Pricing from "../pages/Pricing"
 import ScrollToTop from '../components/ScrollToTop'
@@ -12,8 +10,6 @@ const Routing = () => {
       <ScrollToTop />
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/discover" element={<Discover />} />
-        <Route path="/marketplace" element={<Marketplace />} />
         <Route path='/about' element={<About />} />
         <Route path='/pricing' element={<Pricing />} />
       </Routes>
