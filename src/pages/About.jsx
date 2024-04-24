ca0b762f16e82b58cd87984d09054e4a3b16c553
danie odukwe <kingdanny295@gmail.com>
2024-04-24 01:18:07 +0100

    @fix/ made about page responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,25 +7,25 @@ import { motion } from 'framer-motion'
 const About = () => {
   return (
     <section className="w-full">
-      <div className='px-6 sm:px-8 md:px-12 lg:px-20 xl:px-40'>
+      <div className='px-6 sm:px-8 md:px-12'>
         <div className="w-full flex flex-col md:flex-row items-center justify-between my-10 md:my-20">
-          <div className="flex flex-col gap-5 items-start w-full mb-20">
+          <div className="flex flex-col gap-3 items-start w-full mb-20">
             <motion.h1
             initial={{ opacity: 0, x: '-100%' }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 1 }} 
-            className="font-bold text-[2rem] md:text-[3rem] lg:text-[4rem] max-w leading-lg:[4.6rem]">
+            className="font-bold text-[2rem] lg:text-[2.5rem] lg:leading-[3rem]">
               About Us
             </motion.h1>
             <motion.p
               initial={{ opacity: 0, y: '100%' }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1.5 }} 
-              className="leading-7 text-neutral-300 text-[1rem] max-w-[600px]">
+              className="leading-5  text-neutral-300 text-[0.8em] max-w-[500px]">
                 Welcome to Dreally, your ultimate networking companion! We{"'"}re here to revolutionize the way you connect, showcase your talents, and elevate your professional presence online. With sleek digital business cards, customizable portfolios, and seamless networking tools, Dreally empowers you to make meaningful connections and stand out in a crowded digital landscape. Join us and unlock new opportunities for success. Together, let{"'"}s redefine networking
             </motion.p>
             <button 
-              className="py-4 my-5 px-24 border-none cursor-pointer text-white outline-none rounded-[100px] bg-primary-500"
+              className="py-4 my-3 px-8 text-[0.7rem] border-none cursor-pointer text-white outline-none rounded-[100px] bg-primary-500"
             >
               Sign Up, its free!
             </button>
@@ -35,11 +35,11 @@ const About = () => {
               transition={{ duration: 1.5 }} 
               className='flex items-center gap-4'>
                 <div className='flex items-center'>
-                  <img className='max-w-[200px]' src={group} alt="group" />
+                  <img className='w-[200px]' src={group} alt="group" />
                 </div>
                 <div className='flex flex-col'>
-                  <h1 className='font-semibold text-3xl font-Sora'>2000+</h1>
-                  <p className='text-neutral-300 font-Sora'>Portfolios</p>
+                  <h1 className='font-semibold  font-Sora'>2000+</h1>
+                  <p className='text-neutral-300 font-Sora text-[0.8rem]'>Portfolios</p>
                 </div>
             </motion.div>
           </div>
@@ -57,22 +57,23 @@ const About = () => {
         </motion.div>
         </div>
 
-        <div className='my-10 w-full flex flex-wrap items-start justify-center gap-16'>
-          {why.map((data) => (
-            <div key={data.id} className='flex flex-col gap-4 px-6 lg:px-8 py-4 lg:py-6 min-h-[310px] box bg-primary-50 rounded-2xl cursor-pointer hover:shadow-md hover:bg-primary-100 transition-all'>
-              <img src={data.icon} alt="icons" className='w-[50px] h-[50px] object-cover '/>
-              <h1 className='font-semibold text-xl lg:text-2xl text-primary-800'>{data.title}</h1>
-              <motion.p
-                initial={{ opacity: 0, y: "50%" }}
-                whileInView={{ opacity: 1, y: 0 }}
-                viewport={{ once: true }}
-                transition={{ duration: 1 }} 
-                className='max-w-[350px] text-primary-800 text-[0.95rem] lg:text-[1rem] leading-6'>
-                  {data.text}
-              </motion.p>
-            </div>
-          ))}
-        </div>
+        <div className='my-20 w-full flex-wrap flex items-start justify-center gap-8'>
+        {why.map((data) => (
+          <div key={data.id} className='flex flex-col gap-3 px-5  py-5  min-h-max box bg-primary-50 rounded-2xl cursor-pointer hover:shadow-md hover:bg-primary-100 transition-all'>
+            <img src={data.icon} alt="icons" className='w-[30px] h-[30px] object-cover '/>
+            <h1 className='font-semibold text-base text-primary-800'>{data.title}</h1>
+            <motion.p
+              initial={{ opacity: 0, y: "50%" }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 1 }}   
+              className='max-w-[300px] text-primary-800 text-[0.8rem] leading-5'
+              >
+                {data.text}
+            </motion.p>
+          </div>
+        ))}
+      </div>
       </div>
 
       <GetStarted />
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -21,8 +21,8 @@ const Pricing = () => {
         <div className='px-4 sm:px-8 md:px-12 lg:px-20 xl:px-40 flex flex-col items-center text-center gap-8'>
             <div className="flex flex-col gap-7">
                 <p className="text-primary-400 font-semibold">Pricing</p>
-                <h1 className="font-bold text-[2rem] md:text-[3rem] lg:text-[4rem] text-[#170F49]">Dreally Pricing</h1>
-                <p className="max-w-[700px] leading-6">Unlock the full potential of your networking experience with Dreally. Choose the plan that fits your needs and start connecting, showcasing, and growing today.</p>
+                <h1 className="font-bold text-[2rem] text-[#170F49]">Dreally Pricing</h1>
+                <p className="max-w-[700px] text-[0.85rem] leading-6">Unlock the full potential of your networking experience with Dreally. Choose the plan that fits your needs and start connecting, showcasing, and growing today.</p>
             </div>
 
             <div className="flex items-center gap-6">
@@ -37,28 +37,28 @@ const Pricing = () => {
 
             <div className="w-full flex items-center flex-wrap justify-center gap-6">
                 {/* free pricing card */}
-                <div className="w-full max-w-[300px] py-6 px-8 shadow-xl rounded-3xl flex flex-col items-start ">
+                <div className="w-full max-w-[250px] py-6 px-6 shadow-xl rounded-3xl flex flex-col items-start ">
                     <div className="flex items-center gap-5 mt-5">
-                        <img src={freeBg} alt="" />
+                        <img className="w-[50px]" src={freeBg} alt="" />
                         <div className="flex flex-col items-start gap-3">
-                            <p className="text-neutral-300 text-[0.85rem] font-semibold">For individuals</p>
+                            <p className="text-neutral-300 text-[0.7rem] font-semibold">For individuals</p>
                             <h1>Free</h1>
                         </div>
                     </div>
-                    <p className="text-neutral-300 capitalize text-justify leading-6 text-[0.8rem] my-6 font-Sora">
+                    <p className="text-neutral-300 capitalize text-justify leading-5 text-[0.8rem] my-6 font-Sora">
                         For your personalized dreally profile: streamline your online presence with just one link
                     </p>
-                    <div className="mb-3 text-neutral-300">
-                        <span className="font-bold text-[3rem] text-neutral-600 mr-2">{price === true ? '$' : 'N' }0</span> /monthly
+                    <div className="mb-3 text-neutral-300 text-[0.8rem]">
+                        <span className="font-bold text-[2.5rem] text-neutral-600 mr-2">{price === true ? '$' : 'N' }0</span> /monthly
                     </div>
-                    <p className="my-3 text-neutral-600 font-bold text-[0.9rem]">What{"'"}s included</p>
+                    <p className="my-3 text-neutral-600 font-bold text-[0.8rem]">What{"'"}s included</p>
                     <div className="my-4">
                         {seeMore === false && (
                             <div className="flex flex-col gap-4">
                                 {freeDefault.map((items) => (
                                     <div key={items.id} className="flex items-center gap-3">
-                                        <img className="w-[20px] h-[20px] object-cover" src={items.img} alt="check" />
-                                        <p className="font-medium text-[0.9rem]">{items.text}</p>
+                                        <img className="w-[15px] h-[15px] object-cover" src={items.img} alt="check" />
+                                        <p className="font-medium text-[0.8rem]">{items.text}</p>
                                     </div>
                                 ))}
                             </div>
@@ -68,41 +68,41 @@ const Pricing = () => {
                             <div className="flex flex-col gap-4">
                             {freeExtended.map((items) => (
                                 <div key={items.id} className="flex items-center gap-3">
-                                    <img className="w-[20px] h-[20px] object-cover" src={items.img} alt="check" />
-                                    <p className="font-medium text-[0.9rem]">{items.text}</p>
+                                    <img className="w-[15px] h-[15px] object-cover" src={items.img} alt="check" />
+                                    <p className="font-medium text-[0.8rem]">{items.text}</p>
                                 </div>
                             ))}
                         </div>
                         )}
                     </div>
-                    <span onClick={() => setSeeMore((prev) => (!prev))} className="font-bold justify-center flex my-4 cursor-pointer text-[0.9rem] ml-8">{seeMore === false ? 'See all features' : 'See less features'}</span>
+                    <span onClick={() => setSeeMore((prev) => (!prev))} className="font-bold justify-center flex my-4 cursor-pointer text-[0.8rem] ml-8">{seeMore === false ? 'See all features' : 'See less features'}</span>
                     <Link className="w-full" to='http://app.dreally.com/login'>
-                        <button className="w-full cursor-pointer rounded-[50px] border-none bg-primary-500 text-white p-6 font-semibold text-[1rem]">Get Started</button>
+                        <button className="w-full cursor-pointer rounded-[50px] border-none bg-primary-500 text-white p-4 font-semibold text-[0.8rem]">Get Started</button>
                     </Link>
                 </div>
                 {/* pro plan card */}
                 <div className="w-full relative max-w-[300px] py-6 px-6 rounded-3xl flex flex-col items-start bg-[#6666FF]">
                     <div className="flex items-center gap-5 mt-5">
-                        <img src={paidBg} alt="paidBg" />
+                        <img className="w-[50px]" src={paidBg} alt="paidBg" />
                         <div className="flex flex-col items-start gap-3">
-                            <p className="text-gray-300 text-[0.85rem] font-semibold">For Professionals</p>
+                            <p className="text-gray-300 text-[0.7rem] font-semibold">For Professionals</p>
                             <h1 className="text-gray-100">Premium</h1>
                         </div>
                     </div>
-                    <div className="text-gray-300 capitalize text-justify leading-6 text-[0.8rem] my-6 font-Sora whitespace-normal w-full">
+                    <div className="text-gray-300 capitalize text-justify leading-5 text-[0.8rem] my-6 font-Sora whitespace-normal w-full">
                         For content creators, Entrepreneurs and businesses Advanced analytics, automatic link apps and premium support.
                     </div>
-                    <div className="mb-3 text-gray-300">
-                        <span className="font-bold text-[3rem] text- mr-2 text-gray-100">{price === true ? '$' : 'N' }{price === true ? '0.99' : '1000' }</span> /monthly
+                    <div className="mb-3 text-gray-300 text-[0.8rem]">
+                        <span className="font-bold text-[2.5rem] text- mr-2 text-gray-100">{price === true ? '$' : 'N' }{price === true ? '0.99' : '1000' }</span> /monthly
                     </div>
-                    <p className="my-3 text-gray-100 font-bold text-[0.9rem]">What{"'"}s included</p>
+                    <p className="my-3 text-gray-100 font-bold text-[0.8rem]">What{"'"}s included</p>
                     <div className="my-4">
                         {seeMore === false && (
                             <div className="flex flex-col gap-4">
                                 {paidDefault.map((items) => (
                                     <div key={items.id} className="flex items-center gap-3">
-                                        <img className="w-[20px] h-[20px] object-cover" src={items.img} alt="check" />
-                                        <p className="font-medium text-[0.9rem] text-gray-300">{items.text}</p>
+                                        <img className="w-[15px] h-15px] object-cover" src={items.img} alt="check" />
+                                        <p className="font-medium text-[0.8rem] text-gray-300">{items.text}</p>
                                     </div>
                                 ))}
                             </div>
@@ -112,8 +112,8 @@ const Pricing = () => {
                             <div className="flex flex-col gap-4">
                             {paidExtended.map((items) => (
                                 <div key={items.id} className="flex items-center gap-3">
-                                    <img className="w-[20px] h-[20px] object-cover" src={items.img} alt="check" />
-                                    <p className="font-medium text-[0.9rem] text-gray-300">{items.text}</p>
+                                    <img className="w-[15px] h-[15px] object-cover" src={items.img} alt="check" />
+                                    <p className="font-medium text-[0.8rem] text-gray-300">{items.text}</p>
                                 </div>
                             ))}
                         </div>
@@ -121,7 +121,7 @@ const Pricing = () => {
                     </div>
                     <span onClick={() => setSeeMore((prev) => (!prev))} className="font-bold justify-center flex my-4 cursor-pointer text-[0.9rem] text-gray-300 ml-8">{seeMore === false ? 'See all features' : 'See less features'}</span>
                     <Link className="w-full" to='https://app.dreally.com/admin/choose-payment-type/2'>
-                        <button className="w-full cursor-pointer rounded-[50px] border-none bg-white p-6  text-[1rem] text-primary-500">Try Pro for 7 days</button>
+                        <button className="w-full cursor-pointer rounded-[50px] border-none bg-white p-4  text-[0.8rem] text-primary-500">Try Pro for 7 days</button>
                     </Link>
                     {/* absolute tag on top of the card */}
                     <div className="absolute top-3 right-4 bg-primary-400 text-[#FFFFFF] px-5 py-2 rounded-md text-[0.7rem] font-thin font-Sora">
